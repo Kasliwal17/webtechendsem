a8142c57fc2edcac31175881a63fc8382fbcd801
Aditya Kasliwal <kasliwaladitya17@gmail.com>
2023-11-17 08:41:33 +0530

    Update submit_form.php

--- endsem/q2/submit_form.php
+++ endsem/q2/submit_form.php
@@ -1,43 +1,21 @@
 <?php
-// Replace with your actual database credentials
-$dbHost = 'localhost';
-$dbUsername = 'root'; // default XAMPP username
-$dbPassword = ''; // default XAMPP password is empty
-$dbName = 'first'; // replace with your database name
-
-// Create connection with the MySQL database
-$conn = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
 // Check if the form is submitted
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    // Retrieve form data and escape variables for security
-    $firstname = $conn->real_escape_string($_POST['firstname']);
-    $lastname = $conn->real_escape_string($_POST['lastname']);
-    $email = $conn->real_escape_string($_POST['email']);
-    $phone = $conn->real_escape_string($_POST['phone']);
-    $country = $conn->real_escape_string($_POST['country']);
-    $username = $conn->real_escape_string($_POST['username']);
-    $password = $conn->real_escape_string($_POST['password']);
-    
-    // Hash the password for security
-    $passwordHash = password_hash($password, PASSWORD_DEFAULT);
-    
-    // Insert new entry into the database
-    $insert = $conn->prepare("INSERT INTO users (firstname, lastname, email, phone, country, username, password) VALUES (?, ?, ?, ?, ?, ?, ?)");
-    $insert->bind_param("sssssss", $firstname, $lastname, $email, $phone, $country, $username, $passwordHash);
-    
-    if ($insert->execute()) {
-        echo "Form submitted successfully!";
-    } else {
-        echo "Error: " . $insert->error;
-    }
-    $insert->close();
-}
+    // Create an associative array to store form data
+    $formData = [
+        'firstname' => $_POST['firstname'],
+        'lastname' => $_POST['lastname'],
+        'email' => $_POST['email'],
+        'phone' => $_POST['phone'],
+        'country' => $_POST['country'],
+        'username' => $_POST['username'],
+        'password' => $_POST['password'] // In a real application, you would never store plain-text passwords
+    ];
 
-$conn->close();
+    // Normally you would save this array to a database or a file
+    // For demonstration purposes, we'll just display the array
+    echo '<pre>';
+    print_r($formData);
+    echo '</pre>';
+}
 ?>
